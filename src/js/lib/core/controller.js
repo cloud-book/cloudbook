f928177396e30719652a5e09ec22de210da7cf98
raurodse <raurodse@gmail.com>
2015-03-17 18:27:52 +0100

    refactor. Controller is class contain common functions

--- src/js/lib/core/controller.js
+++ src/js/lib/core/controller.js
@@ -3,12 +3,29 @@ function Controller(){
 }
 
 Controller.prototype.updateSectionName = function(name,cbsectionid) {
-	var core = base.core.getInstance();
-	var ui = base.ui.getInstance();
-	core.updateSectionName(name,cbsectionid);
+	var backend = application.backend.getInstance();
+	var ui = application.ui.getInstance();
+	backend.updateSectionName(name,cbsectionid);
 	ui.updateSectionName(name,cbsectionid);
 };
 
+Controller.prototype.createProProject = function createProProject(name) {
+	CBUtil.include("js/lib/gui/menu.js");
+ 	var backend = application.backend.getInstance()
+ 	var ui = application.ui.getInstance()
+ 	backend.createProject(name);
 
-CBUtil.createNameSpace('base.controller');
-base.controller = CBUtil.singleton(Controller);
+
+ 	backend.loadSectionsObjects();
+ 	ui.renderActionsButtons();
+ 	ui.initSectionsPro();
+};
+
+
+Controller.prototype.createSimpleProject = function createSimpleProject(name) {
+	// body...
+};
+
+
+CBUtil.createNameSpace('application.controller');
+application.controller = CBUtil.singleton(Controller);
