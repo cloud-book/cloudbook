b9e14343eb9132fcaccf5281808892e0bc922b4c
raurodse <raurodse@gmail.com>
2015-05-13 23:53:36 +0200

    creating gettext

--- task/xgettext.js
+++ task/xgettext.js
@@ -1,3 +1,21 @@
+var globule = require('globule');
+var jsxgettext = require('jsxgettext');
+var fs = require('fs');
+
+
 module.exports = function(cb){
-	console.log(x.generate.apply(x,x.generateFromHandlebars({1:fichero},{keyword:'gettext'})))
+
+	var paths = ["src/**/*.*js",
+				 "src/templates/*.*hbs",
+				 "src/**/*.hbs",
+				 "!src/node_modules/**/*.js",
+				 "!src/js/lib_external/**/*.js",
+				 "!src/**/lib_external/**/*.hbs"];
+
+
+
+	files = jsxgettext.generateFromHandlebars({mifichero:fichero}).shift();
+	files['otro'] = fs.readFileSync('importMetadata.js',{encoding:});
+
+	jsxgettext.generate(files,{});
 }
