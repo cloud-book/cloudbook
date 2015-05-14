ba65e8f2fe7f3045e5d076911da1e234e39ae37b
Raul Rodrigo Segura <raurodse@gmail.com>
2015-05-14 12:54:56 +0200

    migrating to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 
 
-gulp.task('xgettext',require('.task/xgettext.js'));
-gulp.task('po2json',require('.task/po2json.js'));
-gulp.task('doc',require('.task/doc.js'));
-gulp.task('nwbuild',require('.task/nwbuild.js'));
+gulp.task('xgettext',require('./task/xgettext.js'));
+gulp.task('po2json',require('./task/po2json.js'));
+gulp.task('doc',require('./task/doc.js'));
+gulp.task('nwbuild',require('./task/nwbuild.js'));
 
--- task/po2json.js
+++ task/po2json.js
@@ -1,3 +1,23 @@
+var globule = require('globule');
+var fs = require('fs');
+var path = require('path');
+var po2json = require('po2json');
+
+
 module.exports = function(cb){
 	
+	var allfiles = [],
+		src = ['./po/**/*.po']
+		dest = 'src/i18n/';
+
+	allfiles = globule.find(src);
+	allfiles.forEach(function(filepath){
+		destpath = path.basename(filepath).slice(0,- 3) + ".js";
+		var result = po2json.parseFileSync(filepath,{format:"jed1.x",pretty:true,stringify:true,"fallback-to-msgid":true});
+		fs.writeFileSync(dest + destpath,"module.exports = "result,{encoding:'utf8'});
+	});
+	
+
+
+
 }
