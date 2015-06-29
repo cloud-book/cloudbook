40159a49e6317ecaf98d697ad7d83d5cbd03226f
Raul Rodrigo Segura <raurodse@gmail.com>
2015-06-29 14:58:20 +0200

    wip in export scorm

--- src/js/lib/core/exportHTMLSplit.js
+++ src/js/lib/core/exportHTMLSplit.js
@@ -2,25 +2,33 @@ function ExportHTMLSplited(){
 
 }
 
-ExportHTMLSplited.prototype.export(destpath){
+ExportHTMLSplited.prototype.exportHTML = function exportHTML(destpath){
 	var fsextra = require('fs-extra'),
-		htmlinfo = {
-			resources:[],
-			items:{}
-		};
+		htmlinfo = {resources:[],items:{}};
 	fsextra.ensureDirSync(destpath);
-	that.renderSection(destpath,'root',htmlinfo);
+	this.renderSection(destpath,'root',htmlinfo);
 	return htmlinfo;
 }
 
-ExportHTMLSplited.prototype.renderSection = function(destpath,sectiontorender,htmlinfo) {
+ExportHTMLSplited.prototype.renderSection = function renderSection(destpath,sectiontorender,htmlinfo) {
 	var storagemanager = application.storagemanager.getInstance();
 	var section = storagemanager.getSectionById(sectiontorender);
-	var filepath = section.idtype + ".html";
+	var fs = require('fs');
+	var path = require('path');
+	var filepath = sectiontorender + ".html";
 	var template = fs.readFileSync('./templates/exporthtmlsplit/section.hbs',{encoding:'utf8'});
+	var templatecompiled = application.util.template.compile(template);
+	var content = "";
+	var that = this;
+	
 	application.util.template.compile(template);
 	section.content.forEach(function(cbobjectid){
 		var cbobject = storagemanager.getCBObjectById(cbobjectid);
-		
+		content += cbobject.htmlView()[0].outerHTML;
 	});
+	var salida = templatecompiled({content:content});
+	fs.writeFileSync(path.join(destpath,filepath),salida);
+	section.sections.forEach(function(cbsectionid){
+		that.renderSection(destpath,cbsectionid,htmlinfo);
+	})
 };
