0425dbadb2e5fc9474666fac8d7ace0a14dfe990
Raul Rodrigo Segura <raurodse@gmail.com>
2015-06-30 14:59:49 +0200

    wip

--- src/js/lib/core/exportHTMLSplit.js
+++ src/js/lib/core/exportHTMLSplit.js
@@ -11,26 +11,29 @@ ExportHTMLSplited.prototype.exportHTML = function exportHTML(destpath){
 }
 
 ExportHTMLSplited.prototype.renderSection = function renderSection(destpath,sectiontorender,htmlinfo) {
-	var storagemanager = application.storagemanager.getInstance();
+	var storagemanager = application.storagemanager.getInstance(),
+		fs = require('fs'),
+		fsextra = require('fs-extra'),
+		path = require('path'),
+		pd = require('pretty-data').pd,
+		content = "",
+		that = this,
+		filepath = sectiontorender + ".html";
 	var section = storagemanager.getSectionById(sectiontorender);
-	var fs = require('fs');
-	var path = require('path');
-	var pd = require('pretty-data').pd;
-	var filepath = sectiontorender + ".html";
 	var template = fs.readFileSync('./templates/exporthtmlsplit/section.hbs',{encoding:'utf8'});
 	var templatecompiled = application.util.template.compile(template);
-	var content = "";
-	var that = this;
 	
-	application.util.template.compile(template);
 	section.content.forEach(function(cbobjectid){
 		var cbobject = storagemanager.getCBObjectById(cbobjectid);
 		content += cbobject.htmlView()[0].outerHTML;
+		var listscripts = Cloudbook.Actions[cbobject.idtype].metadata.external_scripts ? Cloudbook.Actions[cbobject.idtype].metadata.external_scripts.map(function(componentscript){return path.join(Cloudbook.Actions[cbobject.idtype].path,componentscript)}):[];
 	});
-	var salida = pd.xml(templatecompiled({content:content}));
 
+	var salida = pd.xml(templatecompiled({content:content}));
 	fs.writeFileSync(path.join(destpath,filepath),salida);
+
 	section.sections.forEach(function(cbsectionid){
 		that.renderSection(destpath,cbsectionid,htmlinfo);
 	});
+	fsextra.copySync(path.join(Project.Info.projectpath,'rsrc'),path.join(destpath,'rsrc'));
 };
