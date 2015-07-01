1ee8fb56321f58d85ca6a541ec7815df1bb00154
Raul Rodrigo Segura <raurodse@gmail.com>
2015-07-01 14:58:52 +0200

    wip

--- src/js/lib/core/exportHTMLSplit.js
+++ src/js/lib/core/exportHTMLSplit.js
@@ -4,36 +4,73 @@ function ExportHTMLSplited(){
 
 ExportHTMLSplited.prototype.exportHTML = function exportHTML(destpath){
 	var fsextra = require('fs-extra'),
+		path = require('path'),
 		htmlinfo = {resources:[],items:{}};
 	fsextra.ensureDirSync(destpath);
-	this.renderSection(destpath,'root',htmlinfo);
+	htmlinfo.items.root = {};
+	this.renderSection(destpath,'root',htmlinfo.items.root);
+	// copy files from rsrc
+	fsextra.copySync(path.join(Project.Info.projectpath,'rsrc'),path.join(destpath,'rsrc'));
+	// copy files from components
+	htmlinfo.items = htmlinfo.items.root.sections.items;
 	return htmlinfo;
 }
 
-ExportHTMLSplited.prototype.renderSection = function renderSection(destpath,sectiontorender,htmlinfo) {
+ExportHTMLSplited.prototype.renderSection = function renderSection(destpath,cbsectionid,htmlinfo) {
 	var storagemanager = application.storagemanager.getInstance(),
 		fs = require('fs'),
 		fsextra = require('fs-extra'),
 		path = require('path'),
 		pd = require('pretty-data').pd,
-		content = "",
+		templateinfo = {content:"",scripts:[],styles:[]},
 		that = this,
-		filepath = sectiontorender + ".html";
-	var section = storagemanager.getSectionById(sectiontorender);
+		filepath = cbsectionid + ".html";
+	var section = storagemanager.getSectionById(cbsectionid);
 	var template = fs.readFileSync('./templates/exporthtmlsplit/section.hbs',{encoding:'utf8'});
 	var templatecompiled = application.util.template.compile(template);
-	
+	htmlinfo.title = section.name;
+	htmlinfo.filename = filepath;
+	htmlinfo.resourcecode = "R"+cbsectionid;
+	htmlinfo.depends = [];
+	htmlinfo.content = [];
+	if (section.sections.length > 0){
+		htmlinfo.sections = {code:"II" + cbsectionid ,items:{}};
+	}
+
 	section.content.forEach(function(cbobjectid){
 		var cbobject = storagemanager.getCBObjectById(cbobjectid);
-		content += cbobject.htmlView()[0].outerHTML;
-		var listscripts = Cloudbook.Actions[cbobject.idtype].metadata.external_scripts ? Cloudbook.Actions[cbobject.idtype].metadata.external_scripts.map(function(componentscript){return path.join(Cloudbook.Actions[cbobject.idtype].path,componentscript)}):[];
+
+		templateinfo.content += cbobject.htmlView()[0].outerHTML;
+		if (cbobject.triggerHTMLView){
+			templateinfo.content += "<script>" + cbobject.triggerHTMLView() + "</script>";
+		}
+		htmlinfo.depends.push(cbobject.idtype);
+		
 	});
 
-	var salida = pd.xml(templatecompiled({content:content}));
+	htmlinfo.depends = $.unique(htmlinfo.depends);
+	htmlinfo.depends = htmlinfo.depends.filter(function(componentcode){
+		var found = false;
+		if (Cloudbook.Actions[componentcode].metadata.external_scripts){
+			templateinfo.scripts = templateinfo.scripts.concat(Cloudbook.Actions[componentcode].metadata.external_scripts);
+			found = true;
+		}
+
+		if (Cloudbook.Actions[componentcode].metadata.external_css){
+			templateinfo.styles = templateinfo.styles.concat(Cloudbook.Actions[componentcode].metadata.external_css);
+			found = true;
+		}
+		return found;
+	});
+
+
+
+	var salida = pd.xml(templatecompiled(templateinfo));
 	fs.writeFileSync(path.join(destpath,filepath),salida);
 
-	section.sections.forEach(function(cbsectionid){
-		that.renderSection(destpath,cbsectionid,htmlinfo);
+	section.sections.forEach(function(tempcbsectionid){
+		htmlinfo.sections.items["I" + tempcbsectionid] = {};
+		that.renderSection(destpath,tempcbsectionid,htmlinfo.sections.items["I" + tempcbsectionid]);
 	});
-	fsextra.copySync(path.join(Project.Info.projectpath,'rsrc'),path.join(destpath,'rsrc'));
+	
 };
