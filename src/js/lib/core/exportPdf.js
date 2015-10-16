a4610fe17e07f8f5ec4353ee7705d7adc4e982e9
jrpelegrina <juapesai@hotmail.com>
2015-10-16 12:15:15 +0200

    Add a TOC to pdf

--- src/js/lib/core/exportPdf.js
+++ src/js/lib/core/exportPdf.js
@@ -4,8 +4,14 @@
  */
 function ExportPdf() {}
 
+/**
+ * Function to generate an HTML file for each section of the project. 
+ * Returns the list of files generated and information on the project structure 
+ */
+
 ExportPdf.prototype.htmltoPdf = function htmltoPdf(temppath) {
     var fileshtmltopdf=[];
+    var htmlinfo={};
 
     var htmltopdf = new ExportHTMLSplited();
     var infohtml = htmltopdf.exportPDF(temppath);
@@ -13,10 +19,15 @@ ExportPdf.prototype.htmltoPdf = function htmltoPdf(temppath) {
     infohtml.orderedsections.forEach(function(elemento){
         fileshtmltopdf.push(temppath + "/" + elemento.filename);    
     });
+    htmlinfo["sectionsinfo"]=infohtml;
+    htmlinfo["fileshtml"]=fileshtmltopdf;
 
-   return fileshtmltopdf
+    return htmlinfo;
 };
 
+/**
+ * Function to create the tmp dir for the htmlfiles and partial pdf files
+ */
 
 ExportPdf.prototype.createTemppath=function createTemppath(){
     var mktemp = require('mktemp');
@@ -27,17 +38,27 @@ ExportPdf.prototype.createTemppath=function createTemppath(){
 
 };
 
+/**
+ * Function to generate de pdf file
+ */
 ExportPdf.prototype.generatePdf = function generatePdf(parametrospdf) {
     var _this = this;
     var temppath=this.createTemppath();
     var htmlfiles = this.htmltoPdf(temppath.htmlpath);
 
-    this.renderPdf(parametrospdf,htmlfiles,temppath.pdfpath,function(pdffiles){
-       _this.joinPdf(pdffiles,parametrospdf.path);
+
+    this.renderPdf(parametrospdf,htmlfiles.fileshtml,temppath.pdfpath,function(pdffiles){
+       _this.joinPdf(pdffiles,parametrospdf,temppath.pdfpath,htmlfiles.sectionsinfo);
+      
 
     });
+   
     
 };
+
+/**
+ * Function to render the footer template for the pdf files
+ */
 
 ExportPdf.prototype.renderFooter=function renderFooter(footerop){
     var fs = require('fs');
@@ -54,6 +75,9 @@ ExportPdf.prototype.renderFooter=function renderFooter(footerop){
 
 }
 
+/**
+ * Function to render the header template for the pdf files
+ */
 ExportPdf.prototype.renderHeader=function renderHeader(pdfpath,textheader,positionheader){
     var fs = require('fs');
     var headerpdf = "";
@@ -69,7 +93,9 @@ ExportPdf.prototype.renderHeader=function renderHeader(pdfpath,textheader,positi
 
 }
 
-
+/**
+ * Function to set the required parameters of wkhtmltopdf for generating the pdf file
+ */
 ExportPdf.prototype.renderPdf = function renderPdf(parametrospdf, htmlfiles, pdfpath, callback) {
    
     var mktemp = require('mktemp'),
@@ -86,6 +112,8 @@ ExportPdf.prototype.renderPdf = function renderPdf(parametrospdf, htmlfiles, pdf
         pdffiles = [],
         wkop = [],
         tmppdf={};
+        info={};
+       
       
 
     wkop = wkop.concat(['-O', parametrospdf.orientationpage]);
@@ -112,6 +140,9 @@ ExportPdf.prototype.renderPdf = function renderPdf(parametrospdf, htmlfiles, pdf
   
 };
 
+/**
+ * Function to generate the pdf file for each project's section
+ */
 ExportPdf.prototype.generateTemporalPdf = function generateTemporalPdf(tmppdf,callback) {
   var optionsThisFile = [],
       newpage = 0,
@@ -147,17 +178,18 @@ ExportPdf.prototype.generateTemporalPdf = function generateTemporalPdf(tmppdf,ca
       callback(tmppdf.pdffiles);
     } 
     else{
-      if (tmppdf.parametrospdf.positionfooter !== "") {
-        tmppdf.numberpage = tmppdf.numberpage + parseInt(tmppdf.that.getNumberPage(pdf));
-      }
+      tmppdf.numberpage = tmppdf.numberpage + parseInt(tmppdf.that.getNumberPage(pdf,tmppdf.contfiles));
       $("#exportpdfwizard .waitingOK #pdfCount").html(CBI18n.gettext('File') + " " + tmppdf.contfiles + " " + CBI18n.gettext('of') + " " + tmppdf.htmlfiles.length)
-        tmppdf.that.generateTemporalPdf(tmppdf,callback);
+      tmppdf.that.generateTemporalPdf(tmppdf,callback);
     }
   });
   
 };
 
 
+/**
+ * Function to copy the files of the section's resource like flash, video...
+*/
 
 ExportPdf.prototype.copyResources = function copyResources(parametrospdf) {
   var fsextra = require("fs-extra");
@@ -175,10 +207,13 @@ ExportPdf.prototype.copyResources = function copyResources(parametrospdf) {
 };
 
 
-ExportPdf.prototype.getNumberPage=function getNumberPage(pdfpath){
+/**
+ * Function to get the number of page of each pdf file...
+*/
+
+ExportPdf.prototype.getNumberPage=function getNumberPage(pdfpath,orderpdf){
     var spawn = require('child_process').spawnSync;
     var proc = spawn('pdftk',[pdfpath,'dump_data']);
-    var info = {}
     var that=this;
     var page;
     rawdata = proc.stdout.toString().split("\n");
@@ -187,18 +222,110 @@ ExportPdf.prototype.getNumberPage=function getNumberPage(pdfpath){
                that.page=(element.split(":")[1].trim().toString())
             }
     });
+    info[orderpdf]=that.page
+
     return that.page;
 };
 
 
-ExportPdf.prototype.joinPdf=function joinPdf(pdffiles,dest){
+/**
+ * Function to add a Toc and generate the final pdf file
+*/
+
+ExportPdf.prototype.addToc=function addToc(parametrospdf,tmpdir,sectionsinfo,pdfjoined){
+    var CBStorage = application.storagemanager.getInstance();
+    var spawn = require('child_process').spawn;
+    var fsextra = require('fs-extra');
+    var fs = require('fs');
+    var that=this;
+   
+    var tocpdf = "",
+        tocfile="",
+        codeseclength="",
+        codesection="",
+        cbsec="",
+        levelsec="",
+        pagenumber=1; 
+        initpage=1,  
+        i=1,
+        definitivo="", 
+        paramtoc={},
+        toc=[];
+
+       
+    var template = fs.readFileSync('./templates/tocPdf.hbs',{encoding:'utf8'});
+    var templatecompiled = application.util.template.compile(template);
+ 
+   
+    //Generamos el diccionario con los datos necesarios de las secciones para general el TOC
+    paramtoc.infosection=[];
+    info[0]="0";
+    sectionsinfo.orderedsections.forEach(function(elemento){
+       
+        var sections = {};
+        
+        sections.title=elemento.title;
+        
+        pagenumber=pagenumber + parseInt(info[i-1]);
+        sections.pages=pagenumber;
+
+        codeseclength=elemento.filename.indexOf('.');
+        codesection=elemento.filename.substring(0,codeseclength);
+        cbsec=CBStorage.getSectionById(codesection);
+        
+        levelsec=cbsec.numbering.split(".").length;
+        sections.level =levelsec;
+
+        i++;
+        paramtoc.infosection.push(sections);
+    });
+
+    //Creamos el txt con los datos del TOC a añadir al pdf
+    tocpdf = templatecompiled(paramtoc);
+    
+    fs.writeFileSync(tmpdir+"toc.txt", tocpdf);
+    
+    //Generamos el pdf definitivo con el TOC
+    tocfile=tmpdir+"toc.txt";
+    definitivo=parametrospdf.path;
+    toc.push(pdfjoined);
+    toc.push("update_info_utf8")
+    toc.push(tocfile);
+    toc.push("output");
+    toc.push(definitivo);
+
+    var result=spawn("pdftk",toc);
+    result.on('close',function(code){
+      if (fs.existsSync(definitivo)){   
+          $("#exportpdfwizard").find('.waitingPdf').css("display", "none") 
+          $("#exportpdfwizard").dialog("destroy");
+      };
+
+    });
+    result.stderr.on('data',function(data){
+      $("#exportpdfwizard").find('.waitingPdf').css("display", "none");
+      $("#exportpdfwizard").find('.waitingER').css("display", "inline");
+      $("#exportpdfwizard .waitingER #messageError").html("Error : " + data);
+   });
+
+
+
+}
+
+/**
+ * Function to join all pdf files to gen a final pdf file
+ */
+ExportPdf.prototype.joinPdf=function joinPdf(pdffiles,parametrospdf,tmpdir,sectionsinfo){
    var spawn = require('child_process').spawn;
    var fsextra = require('fs-extra');
    var fs = require('fs');
+   var that=this;
+  
+   var pdfjoined=tmpdir+"unido.pdf";
 
    pdffiles.push("cat");
    pdffiles.push("output");
-   pdffiles.push(dest);
+   pdffiles.push(pdfjoined);
 
 
    $("#exportpdfwizard").find('.waitingOK').css("display", "none");
@@ -206,12 +333,11 @@ ExportPdf.prototype.joinPdf=function joinPdf(pdffiles,dest){
    $("#exportpdfwizard").find('.waitingPdf').css("display", "inline") 
    var result = spawn("pdftk",pdffiles);
    result.on('close',function(code){
-      if (fs.existsSync(dest)){    
-        $("#exportpdfwizard").find('.waitingPdf').css("display", "none") 
-        $("#exportpdfwizard").dialog("destroy");
+      if (fs.existsSync(pdfjoined)){   
+         that.addToc(parametrospdf,tmpdir,sectionsinfo,pdfjoined);
       }
    });
-
+   
    result.stderr.on('data',function(data){
       $("#exportpdfwizard").find('.waitingPdf').css("display", "none");
       $("#exportpdfwizard").find('.waitingER').css("display", "inline");
