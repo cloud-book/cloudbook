60d8df0875d5bd79583cae0a70a5053ba93c37fd
Raul Rodrigo Segura <raurodse@gmail.com>
2020-04-03 16:51:50 +0200

    add macos icons

--- src/components/core/video/core.js
+++ src/components/core/video/core.js
@@ -17,7 +17,7 @@ util.inherits(VideoBox,CBobject);
 VideoBox.prototype.editorView = function editorView() {
   var aux = VideoBox.super_.prototype.editorView.call(this);
   var videoelement = $(window.document.createElement('video')).attr('controls','');
-  var source = $(window.document.createElement('source')).attr('src',Project.Info.projectpath + "/rsrc/"+ this.videopath).attr('type',this.videoformat);
+  var source = $(window.document.createElement('source')).attr('src','file://' + Project.Info.projectpath + "/rsrc/"+ this.videopath).attr('type',this.videoformat);
   videoelement.css('height','100%');
   videoelement.css('width','100%');
   videoelement.append(source);
